bd5aece94a1f6db89b9241c655f8f73bbe25e4c4
minxrka <minorka04@mail.ru>
2024-06-20 05:06:50 +0300

    1.0.3 test auth x3

--- src/firebase.js
+++ src/firebase.js
@@ -2,7 +2,6 @@
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 console.log(process.env);
@@ -18,5 +17,3 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-export { app, auth };
